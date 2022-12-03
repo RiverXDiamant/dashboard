26afd910b560f479ef1e6dd98a96a05d85d0bd6a
Alex <eddings.sharanna@gmail.com>
2022-12-03 14:20:44 -0500

    imported Welcome component

--- src/components/WebsiteVisitors.js
+++ src/components/WebsiteVisitors.js
@@ -1,8 +1,14 @@
+// import component
+import Welcome from "./Welcome";
+
 export default function WebsiteVisitors() {
   return (
     <div className="visitor-count">
       <h3>Website Visitors</h3>
       <p>821</p>
+      <div className="welcome">
+        <Welcome />
+      </div>
     </div>
   );
 }
