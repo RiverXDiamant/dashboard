c9d3fc6b88e9b3734c0d6cafe14f1de518bc9694
Alex <eddings.sharanna@gmail.com>
2022-12-02 20:47:32 -0500

    first commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+export default function Sidebar() {
+  return (
+    <div>
+      <aside>
+        <ul>
+          <h2>Dashboard</h2>
+          <h2>Widget</h2>
+          <h2>Reviews</h2>
+          <h2>Customers</h2>
+          <h2>Online Analysis</h2>
+          <h2>Settings</h2>
+        </ul>
+      </aside>
+    </div>
+  );
+}
