41141ac1bc3e4afb4a99ba815f26cdf0d7179329
Alex <eddings.sharanna@gmail.com>
2022-12-03 13:28:06 -0500

    created Sentiment Analysis component

---
+++ src/components/SentimentAnalysis.js
@@ -0,0 +1,10 @@
+export default function SentimentAnalysis() {
+  return (
+    <div className="sentiment">
+      <h3>Sentiment Analysis</h3>
+      <p>960</p>
+      <p>122</p>
+      <p>321</p>
+    </div>
+  );
+}
